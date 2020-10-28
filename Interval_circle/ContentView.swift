3df346e0b899788a46b443c7009b54f9a9a4a975
den0206 <mac.pop@mineo.jp>
2020-10-28 20:41:10 +0900

    Initial Commit

---
+++ Interval_circle/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Interval_circle
+//
+//  Created by 酒井ゆうき on 2020/10/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Interval_circle/Interval_circleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Interval_circleApp.swift
+//  Interval_circle
+//
+//  Created by 酒井ゆうき on 2020/10/28.
+//
+
+import SwiftUI
+
+@main
+struct Interval_circleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
